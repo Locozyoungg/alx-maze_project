c937f2c63a407bc99f46c0082685d694963a9c8e
shikoliamos31 <amosshikoli@gmail.com>
2024-06-06 06:55:51 -0700

    recommender_music

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title= "my webpage", layout= "wide")
-#---Header section---
+#----Header section----
 st.subheader("Music Recomender")
 st.title("music recommender for your favourite music")
 st.write("[Learn more >](https://musicrecomendor.com)")
